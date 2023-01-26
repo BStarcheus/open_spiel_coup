4ab302b5d9ff20c0f29a0722a9b81fd67f758316
Brandon Starcheus <bstarcheus@gmail.com>
2023-01-25 19:32:14 -0500

    Add script to run algos for all flagfiles

---
+++ coup_experiments/README.md
@@ -0,0 +1,5 @@
+# Running algorithms on coup
+
+1. Add flag files to `scripts/flags/` to configure runs. See `scripts/flags/examples/`.
+2. `./run.sh`
+3. Find logs in `data/`
---
+++ coup_experiments/scripts/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+algos=("deep_cfr" "deep_cfr_tf2" "nfsp" "mccfr")
+
+cd /repo/coup_experiments/data
+for algo in "${algos[@]}"; do
+    if [ ! -d "${algo}" ]; then
+        mkdir ${algo}
+    fi
+done
+
+cd /repo/coup_experiments/scripts
+for algo in "${algos[@]}"; do
+    for flagfile in flags/${algo}*.cfg; do
+        [ -f "${flagfile}" ] || continue
+        python3 ${algo}.py --flagfile=${flagfile}
+    done
+done
