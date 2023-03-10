e1cb1d95e81a5564a286d002482c2d500c83332a
Brandon Starcheus <bstarcheus@gmail.com>
2023-03-10 10:03:12 -0500

    Fix new type

--- open_spiel/games/coup.h
+++ open_spiel/games/coup.h
@@ -206,7 +206,7 @@ class CoupGame : public Game {
   int NumPlayers() const override { return kNumPlayers; }
   double MinUtility() const override { return -2; }
   double MaxUtility() const override { return 2; }
-  double UtilitySum() const override { return 0; }
+  absl::optional<double> UtilitySum() const override { return 0; }
   std::vector<int> InformationStateTensorShape() const override;
   std::vector<int> ObservationTensorShape() const override;
 
